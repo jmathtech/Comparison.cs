3af51cb3ac5f321fe78e26a078be1372be21b44d
Jamil Matheny <jamil.matheny@gmail.com>
2020-10-05 13:29:06 -0400

    Add project files.

---
+++ Comparison/Comparison.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ Name: Jamil Matheny
+ Date: 10/05/2020
+ Class: CIS214
+ 
+ */
+
+namespace Comparison
+{
+    public class Comparison
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
