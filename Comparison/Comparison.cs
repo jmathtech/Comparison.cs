0c1b8a9b0f4147f95a052f4ad5bac69297e28d52
Jamil Matheny <36749450+jmathtech@users.noreply.github.com>
2020-10-05 21:55:20 -0400

    Update Comparison.cs

--- Comparison/Comparison.cs
+++ Comparison/Comparison.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+
 
 /* 
  Name: Jamil Matheny
@@ -10,14 +7,42 @@ using System.Threading.Tasks;
  Class: CIS214
  
  */
-
 namespace Comparison
 {
     public class Comparison
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) //
         {
+            int number1; // declare the first number to compare
+            int number2; // declare the second number to compare
 
-        }
-    }
+            // Prompts the user and read the first number
+            Console.Write("Enter the first integer: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            // Prompts the user and read the second number
+            Console.Write("Enter the second integer: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            // If statements with relational and equality operators.
+            if (number1 == number2)
+                Console.WriteLine("{0} == {1}", number1, number2);
+
+            if (number1 != number2)
+                Console.WriteLine("{0} != {1}", number1, number2);
+
+            if (number1 < number2)
+                Console.WriteLine("{0} < {1}", number1, number2);
+
+            if (number1 > number2)
+                Console.WriteLine("{0} > {1}", number1, number2);
+
+            if (number1 <= number2)
+                Console.WriteLine("{0} <= {1}", number1, number2);
+
+            if (number1 >= number2)
+                Console.WriteLine("{0} >= {1}", number1, number2);
+        }   //end Main
+    } //end class Comparison
 }
